d5f2d71a0b75479c9d0189e942a5433a30894d12
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2024-03-17 23:17:33 +0100

    startet spliting vulkan utils

--- src/Core/Renderer/RenderAPI/Vulkan/VulkanAPI.cpp
+++ src/Core/Renderer/RenderAPI/Vulkan/VulkanAPI.cpp
@@ -15,22 +15,6 @@ VulkanAPI::VulkanAPI(GLFWwindow *windowHandle) : m_WindowHandle(windowHandle)
     }
 }
 
-// void VulkanAPI::Init()
-//{
-//
-//     glfwMakeContextCurrent(m_WindowHandle);
-//     int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-//     HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
-//
-//     HZ_CORE_INFO("OpenGL Info:");
-//     HZ_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
-//     HZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
-//     HZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
-//
-//     HZ_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5),
-//                    "Hazel requires at least OpenGL version 4.5!");
-// }
-
 bool VulkanAPI::Init()
 {
     createInstance();
