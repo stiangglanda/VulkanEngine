36a3670727d0ce4764e3c87b01f248c415ced2bb
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2024-03-18 08:36:16 +0100

    added generate_mipmaps to vk_images.h

--- src/Core/Renderer/RenderAPI/Vulkan/vk_images.h
+++ src/Core/Renderer/RenderAPI/Vulkan/vk_images.h
@@ -8,4 +8,6 @@ namespace vkutil {
 void transition_image(VkCommandBuffer cmd, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
 
 void copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destination,VkExtent2D srcSize, VkExtent2D dstSize);
+
+void generate_mipmaps(VkCommandBuffer cmd, VkImage image, VkExtent2D imageSize);
 } // namespace vkutil
