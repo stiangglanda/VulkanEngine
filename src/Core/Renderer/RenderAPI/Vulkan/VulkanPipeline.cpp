f53a3e24463eb0585c97297b981af4bd7a9f9237
stiangglanda <leander.kieweg@gmail.com>
2024-08-23 11:03:02 +0200

    created VulkanPipeline with basic implementation

---
+++ src/Core/Renderer/RenderAPI/Vulkan/VulkanPipeline.cpp
@@ -0,0 +1,16 @@
+#include "VulkanPipeline.h"
+#include "VulkanDebug.h"
+
+namespace Core
+{
+
+void VulkanPipeline::Shutdown()
+{
+    VE_CORE_INFO("Shutdown Vulkan Pipeline");
+}
+
+void VulkanPipeline::Init()
+{
+    VE_CORE_INFO("Successful: Init Vulkan Pipeline");
+}
+}
---
+++ src/Core/Renderer/RenderAPI/Vulkan/VulkanPipeline.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../../../vepch.h"
+#include <vulkan/vulkan.h>
+
+namespace Core 
+{
+
+    class VulkanPipeline 
+    {
+    public:
+        VulkanPipeline() {}
+        ~VulkanPipeline() {}
+
+        void Init();
+
+        void Shutdown();
+
+    private:
+	};
+}
